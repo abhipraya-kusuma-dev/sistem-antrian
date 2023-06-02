41762b6e506ebf3d0f0064f8c403ece8278cd901
Octagram <zerorangers81@gmail.com>
2023-06-02 16:17:32 +0700

    style: pak dicky demen tytd

--- resources/views/antrian/daftar.blade.php
+++ resources/views/antrian/daftar.blade.php
@@ -18,13 +18,13 @@
   </header>
 
   <main class="flex flex-col items-center space-y-10 p-4 ">
-    <h1 class="text-5xl font-bold">Pilih Jenjang Antrean </h1>
-    <div class="flex flex-col space-y-8 text-center w-full">
+    <h1 class="text-5xl font-bold drop-shadow-lg">Pilih Jenjang Antrean</h1>
+    <div class="flex flex-col space-y-8 text-center w-full text-4xl text-stroke text-stroke-black">
       @foreach($jenjang as $j)
-    <a href="/antrian/daftar/konfirmasi/{{ $j }}" class="py-3 px-4 font-semibold text-white text-xl uppercase rounded-full border-2 shadow-xl shadow-slate-400 border-black" style="background-color: {{ $warna[$loop->index] }};">
+    <a href="/antrian/daftar/konfirmasi/{{ $j }}" class="py-3 px-4 font-semibold text-white uppercase rounded-full border-2 shadow-xl shadow-slate-400 border-black" style="background-color: {{ $warna[$loop->index] }};">
       {{ $j }}</a>
     @endforeach
-    <a href="/antrian/daftar/konfirmasi/bendahara" class="py-3 px-4 font-semibold text-white text-xl uppercase rounded-full border-2 shadow-xl shadow-slate-400 border-black" style="background-color: {{ $warna[4] }};">Bendahara</a>
+    <a href="/antrian/daftar/konfirmasi/bendahara" class="py-3 px-4 font-semibold text-white uppercase rounded-full border-2 shadow-xl shadow-slate-400 border-black" style="background-color: {{ $warna[4] }};">Bendahara</a>
 
     </div>
 
