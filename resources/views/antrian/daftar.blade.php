9ce5cc3cd9ee162e7c2d6337f1362684eab78ce7
Octagram <zerorangers81@gmail.com>
2023-06-02 15:50:00 +0700

    style: tytyd adi kecil

--- resources/views/antrian/daftar.blade.php
+++ resources/views/antrian/daftar.blade.php
@@ -12,19 +12,20 @@
   @endif
 
 
-  <header class="flex p-6 items-center space-x-4">
+  <header class="flex p-6 items-center space-x-4 flex-col space-y-4">
     <img src="{{asset('wk.png')}}" class="w-20" alt="logo">
-    <p class="text-[#1A508B] font-bold text-5xl uppercase">Antrean PPDB</p>
+    <p class="text-[#1A508B] font-bold text-3xl uppercase">Antrean PPDB</p>
   </header>
 
-  <main class="flex flex-col items-center space-y-4 p-4 mt-20">
-    <h1 class="text-5xl font-bold">Daftar Antrean Jenjang </h1>
-    <div class="flex flex-col space-y-4 text-center w-full">
+  <main class="flex flex-col items-center space-y-10 p-4 ">
+    <h1 class="text-5xl font-bold">Pilih Jenjang Antrean </h1>
+    <div class="flex flex-col space-y-8 text-center w-full">
       @foreach($jenjang as $j)
-    <a href="/antrian/daftar/konfirmasi/{{ $j }}" class="py-3 px-4 font-semibold text-white text-xl uppercase" style="background-color: {{ $warna[$loop->index] }};">
+    <a href="/antrian/daftar/konfirmasi/{{ $j }}" class="py-3 px-4 font-semibold text-white text-xl uppercase rounded-full border-2 shadow-xl shadow-slate-400 border-black" style="background-color: {{ $warna[$loop->index] }};">
       {{ $j }}</a>
-      <a href="/antrian/daftar/konfirmasi/antrian/bendahara" class="py-3 px-4 font-semibold text-white text-xl uppercase" style="background-color: {{ $warna }};"></a>
     @endforeach
+    <a href="/antrian/daftar/konfirmasi/bendahara" class="py-3 px-4 font-semibold text-white text-xl uppercase rounded-full border-2 shadow-xl shadow-slate-400 border-black" style="background-color: {{ $warna[4] }};">Bendahara</a>
+
     </div>
 
   </main>
