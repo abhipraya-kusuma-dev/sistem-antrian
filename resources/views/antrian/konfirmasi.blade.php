172c868a824aacb60f8da6c7779a8316d9da3955
Octagram <zerorangers81@gmail.com>
2023-06-04 20:51:27 +0700

    style: operator antrean, ticket, and add logic at 'terpanggil' section

--- resources/views/antrian/konfirmasi.blade.php
+++ resources/views/antrian/konfirmasi.blade.php
@@ -4,10 +4,10 @@
 <div class="h-screen flex items-center justify-center">
   <div class="p-4 border-2 border-black">
     <h1 class="text-lg">Nomor Antrian anda saat ini adalah</h1>
-    <p class="font-bold text-center">Nomor {{ $nomorAntrianSaatIni }}</p>
+    <p class="font-bold text-center text-4xl">Nomor {{ $nomorAntrianSaatIni }}</p>
 
-    <div class="flex mt-2">
-      <a href="/antrian/daftar" class="active:bg-red-800 py-2 px-4 w-1/2 bg-red-500/80 text-center text-white font-bold">Nanti Aja Deh</a>
+    <div class="flex mt-2 space-x-4">
+      <a href="/antrian/daftar" class="active:bg-red-800 py-2 px-4 w-1/2 bg-red-500/80 text-center text-white font-bold">Kembali</a>
       <form class="w-1/2" action="/antrian/daftar/proses" method="post">
         @csrf
         <input type="hidden" name="nomor_antrian" value="{{ $nomorAntrianSaatIni }}" />
--- resources/views/operator/jenjang.blade.php
+++ resources/views/operator/jenjang.blade.php
@@ -1,21 +1,23 @@
 @extends('layout.main')
 
 @section('content')
-<div>
+<div class="p-8">
 
   <div>
-    <h1 class="text-2xl font-bold">Pilih Tanggal Pendaftaran (default nya hari ini)</h1>
-    <form class="mt-2">
-      <input type="date" name="tanggal_pendaftaran" value="{{ $tanggal_pendaftaran }}" />
-      <button type="submit">Pilih Tanggal</button>
+    <div class="note bg-blue-400 p-4 text-white">
+      <h1 class="text-xl font-bold"><span class="text-yellow-300">NOTE:</span> Pilih Tanggal Pendaftaran (default tanggal hari ini)</h1>
+    </div>
+    <form class="mt-4">
+      <input type="date" name="tanggal_pendaftaran" class="border-2 border-black px-4 py-1.5" value="{{ $tanggal_pendaftaran }}" />
+      <button type="submit" class="bg-green-400 text-white px-4 py-1.5 font-semibold">Pilih Tanggal</button>
     </form>
   </div>
 
-  <nav class="flex space-x-2">
-    <a href="/operator/antrian/jenjang/{{ $jenjang }}/belum?tanggal_pendaftaran={{ $tanggal_pendaftaran }}" class="{{ $status === 'belum' ? 'text-blue-600 font-bold' : 'text-blue-600/80' }}">
+  <nav class="flex space-x-2 mt-8">
+    <a href="/operator/antrian/jenjang/{{ $jenjang }}/belum?tanggal_pendaftaran={{ $tanggal_pendaftaran }}" class="{{ $status === 'belum' ? 'text-blue-600 border-b-4 border-blue-600 font-bold' : 'text-blue-600/80' }}">
       Belum Terpanggil
     </a>
-    <a href="/operator/antrian/jenjang/{{ $jenjang }}/sudah?tanggal_pendaftaran={{ $tanggal_pendaftaran }}" class="{{ $status === 'sudah' ? 'text-blue-600 font-bold' : 'text-blue-600/80' }}">
+    <a href="/operator/antrian/jenjang/{{ $jenjang }}/sudah?tanggal_pendaftaran={{ $tanggal_pendaftaran }}" class="{{ $status === 'sudah' ? 'text-blue-600 font-bold border-b-4 border-blue-600' : 'text-blue-600/80' }}">
       Terpanggil
     </a>
     <a href="/operator/antrian/jenjang/{{ $jenjang }}/lewati?tanggal_pendaftaran={{ $tanggal_pendaftaran }}" class="{{ $status === 'lewati' ? 'text-blue-600 font-bold' : 'text-blue-600/80' }}">
@@ -23,13 +25,13 @@
     </a>
   </nav>
 
-  <h2 class="text-lg mt-2 font-bold">Antrian</h2>
+  <h2 class="text-2xl mt-2 font-bold">Antrian</h2>
 
-  <ul class="text-green-600">
+  <ul class="text-green-600 border-2 border-black grid grid-cols-3 gap-4 text-xl p-4 mt-4">
     @if(count($semua_antrian))
 
     @foreach($semua_antrian as $antrian)
-    <li>
+    <li class="border-2 border-black text-center">
       <a href="/operator/antrian/panggil/{{ $antrian->id }}">
         <b>{{ $antrian->nomor_antrian }}</b>
       </a>
