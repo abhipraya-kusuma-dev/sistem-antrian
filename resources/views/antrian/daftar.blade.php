56902b73eefc2ef40766d35684f3219e8245c872
AdiCahyaSaputra <adi.jsonparse@gmail.com>
2023-06-05 14:44:34 +0700

    feat: regenerate after registration successful

--- resources/views/antrian/daftar.blade.php
+++ resources/views/antrian/daftar.blade.php
@@ -34,6 +34,7 @@
   </main>
 
 </div>
+<script src="https://cdn.socket.io/4.5.4/socket.io.min.js"></script>
 <script>
   const daftarContainer = document.getElementById('daftar-container')
   if (daftarContainer.firstElementChild.id === 'message') {
@@ -42,4 +43,11 @@
     }, 3000)
   }
 </script>
+
+@if(session('create-success'))
+<script>
+  const socket = io(`{{ env('SOCKET_IO_SERVER') }}`)
+  socket.emit('skip antrian', 'skip')
+</script>
+@endif
 @endsection
